b0990d5404279894c53bfaa08481f4c3870ed3eb
SnIvan <ivan.sakhan@icloud.com>
2019-04-03 16:13:41 -0400

    Initial Commit

---
+++ SkeletonShooter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonShooter
+//
+//  Created by Ivan on 2019-04-03.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
